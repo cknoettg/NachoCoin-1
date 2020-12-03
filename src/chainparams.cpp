c12ea1fde650b0839800dbfb46b9d55e07e8b617
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-12-03 16:32:09 +0000

    Wallet frame made lighter and more realistic

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -135,7 +135,7 @@ public:
         // BIP44 coin type is from https://github.com/satoshilabs/slips/blob/master/slip-0044.md
         base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0xde).convert_to_container<std::vector<unsigned char> >();
 
-vSeeds.push_back(CDNSSeedData("nachcoin.tk", "nachocoin.tx"));
+vSeeds.push_back(CDNSSeedData("nachcoin.tk", "nachocoin.tk"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
